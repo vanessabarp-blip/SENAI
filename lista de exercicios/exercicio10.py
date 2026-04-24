8182d466917211ae491a6d81b3d97073e0a21e12
vanessabarp-blip <vanessa_barp@estudante.sesisenai.org.br>
2026-04-24 20:02:05 +0000

    update

--- lista de exercicios/exercicio10.py
+++ lista de exercicios/exercicio10.py
@@ -1,9 +1,18 @@
-qtd = int(input("Quantidade de sanduíches: "))
 
-queijo = (qtd * 100) / 1000
-presunto = (qtd * 50) / 1000
-carne = (qtd * 100) / 1000
+quantidade = int(input("Digite a quantidade de sanduíches: "))
 
-print("Queijo (kg):", queijo)
-print("Presunto (kg):", presunto)
-print("Carne (kg):", carne)
+# Cálculo direto (Peso em gramas convertido para quilos)
+# Queijo: 2 fatias de 50g = 100g por sanduíche
+queijo_kg = (quantidade * 100) / 1000
+
+# Presunto: 1 fatia de 50g = 50g por sanduíche
+presunto_kg = (quantidade * 50) / 1000
+
+# Carne: 1 rodela de 100g = 100g por sanduíche
+carne_kg = (quantidade * 100) / 1000
+
+# Saída dos resultados
+print("Para fazer", quantidade, "sanduíches, você precisa de:")
+print(queijo_kg, "kg de queijo")
+print(presunto_kg, "kg de presunto")
+print(carne_kg, "kg de carne")
