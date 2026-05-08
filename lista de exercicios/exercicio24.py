1a279111c356d022f25d8b875ac3af9e6a70885f
vanessabarp-blip <vanessa_barp@estudante.sesisenai.org.br>
2026-05-08 18:02:29 +0000

    update

--- lista de exercicios/exercicio24.py
+++ lista de exercicios/exercicio24.py
@@ -1,9 +1,7 @@
-# Recebe as duas notas
+# Atividade 28 - Simples
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 
-# Calcula a média ponderada (peso 2 e 3)
-media = (nota1 * 2 + nota2 * 3) / (2 + 3)
+media = ((nota1 * 2) + (nota2 * 3)) / 5
 
-# Mostra o resultado
-print("A média ponderada é:", media)
+print(f"A média ponderada é: {media:.2f}")
